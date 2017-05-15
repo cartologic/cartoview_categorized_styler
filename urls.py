ab787ade077276ab062d3b84e956ae602a7c8586
Kamal Alseisy <kamal.alseisy@cartologic.com>
2017-05-15 15:13:51 +0200

    add missing proxy code to get sld

--- urls.py
+++ urls.py
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
    url(r'^$', views.index, name='%s.index' % APP_NAME),
    url(r'^styles/(?P<layername>[^/]*)$', views.layer_styles, name='%s.layer_styles' % APP_NAME),
    url(r'^styles/save/(?P<layer_name>[^/]*)/(?P<style_name>[^/]*)$', views.save_style, name='%s.save_style' % APP_NAME),
+   url(r'^proxy/geoserver/rest/(?P<suburl>.*)$', views.geoserver_rest_proxy, name='%s.proxy' % APP_NAME),
 )
--- views.py
+++ views.py
@@ -56,3 +56,58 @@ def save_style(request, layer_name, style_name):
     # except:
     #     res["success"] = False
     return HttpResponse(json.dumps(res), content_type="text/json")
+
+#gs_catalog = Catalog(ogc_server_settings.internal_rest, username, password)
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+@login_required
+def geoserver_rest_proxy(request, suburl):
+    url = ogc_server_settings.LOCATION + "rest/" + suburl
+    username, password = ogc_server_settings.credentials
+
+    requests_args = dict()
+    headers = get_headers(request.META)
+
+    requests_args['headers'] = {}
+    requests_args['data'] = request.body
+
+
+    # Overwrite any headers and params from the incoming request with explicitly
+    # specified values for the requests library.
+    headers.update(requests_args['headers'])
+
+    params = request.GET.copy()
+    params.update(QueryDict('', mutable=True))
+    requests_args['params'] = params
+    # If there's a content-length header from Django, it's probably in all-caps
+    # and requests might not notice it, so just remove it.
+    for key in headers.keys():
+        if key.lower() == 'content-length':
+            del headers[key]
+
+    requests_args['headers'] = headers
+
+
+    response = requests.request(request.method, url, auth=(username, password), stream=True, **requests_args)
+    kwargs = dict(status=response.status_code)
+    if "Content-Type" in response.headers:
+        kwargs["content_type"] = response.headers["Content-Type"]
+    proxy_response = HttpResponse(response.content, **kwargs)
+
+
+    return proxy_response
+
+def get_headers(environ):
+    """
+    Retrieve the HTTP headers from a WSGI environment dictionary.  See
+    https://docs.djangoproject.com/en/dev/ref/request-response/#django.http.HttpRequest.META
+    """
+    headers = {}
+    for key, value in environ.iteritems():
+        # Sometimes, things don't like when you send the requesting host through.
+        if key.startswith('HTTP_') and key != 'HTTP_HOST':
+            headers[key[5:].replace('_', '-')] = value
+        elif key in ('CONTENT_TYPE', 'CONTENT_LENGTH'):
+            headers[key.replace('_', '-')] = value
+
+    return headers
