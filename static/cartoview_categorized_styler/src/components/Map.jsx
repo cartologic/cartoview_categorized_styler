75be0f9b8a910eeb098f55d3e17c2388d7949dbf
Khaled Boka <bokae97@gmail.com>
2018-04-23 13:11:48 +0200

    fixed: add wps urls with session token, proxy url undefined instead of null

--- static/cartoview_categorized_styler/src/components/Map.jsx
+++ static/cartoview_categorized_styler/src/components/Map.jsx
@@ -7,7 +7,7 @@ export default class Map extends React.Component {
     super(props);
     const {layerName, styleName} = props.config;
     this.layerSource = new ol.source.ImageWMS({
-      url: URLS.geoserver + "wms",
+      url: URLS.wmsURL, 
       params: {
         LAYERS: layerName,
         STYLES: styleName,
