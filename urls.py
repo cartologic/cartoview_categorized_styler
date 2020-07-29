7295833d00b58967d47a33249c3b36371a260535
Mohamed Radwan <mohamed.radwan@cartologic.com>
2020-07-29 12:06:08 +0200

    Fix wrong geoserver request by save_style POST url

--- urls.py
+++ urls.py
@@ -5,6 +5,6 @@ from . import views, APP_NAME
 urlpatterns = [
     re_path(r'^$', views.index, name='%s.index' % APP_NAME),
     path('styles/<str:layername>/', views.layer_styles, name='%s.layer_styles' % APP_NAME),
-    path('styles/save/<str:layer_name>/<str:style_name>/', views.save_style, name='%s.save_style' % APP_NAME),
+    path('styles/save/<str:layer_name>/<str:style_name>', views.save_style, name='%s.save_style' % APP_NAME),
     re_path(r'^proxy/geoserver/rest/(?P<suburl>.*)$', views.geoserver_rest_proxy, name='%s.proxy' % APP_NAME),
 ]
